d1802bb2c1165378423579b2617e95555cae267f
vinayk16 <72502726+vinayk16@users.noreply.github.com>
2023-06-12 16:08:14 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "https://amazon-backend-app.herokuapp.com",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://backend-phvv.onrender.com:5000",
   // baseURL: "https://epicraft-backend.onrender.com"
   // baseURL: process.env.BASE_URL,
 });
